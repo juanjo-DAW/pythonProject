090f312292f8eb083621c89fdf52988347739174
juanjo-DAW <jarnau@institutmvm.cat>
2021-12-02 15:42:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def main():
+    lst = list()
+    x = int(input("Introdueix el valor del rang de la teva llista: "))
+
+    for x in lst():
+        lst.append("Introdueix un valor per la teva llista:")
+
+if __name__ == '__main__':
+    main()
