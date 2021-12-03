4ef8e1d06a3a9dd5c1951c393547f32a741d3f50
juanjo-DAW <jarnau@institutmvm.cat>
2021-12-03 17:00:33 +0100

    Initial code

--- main2.py
+++ main2.py
@@ -9,6 +9,7 @@ def main():
     tflst = list()
     edadlst = list()
     contactolst = list()
+    # valorlst = list()
     i = 0
 
 
@@ -22,30 +23,37 @@ def main():
         cognom2 = input("Introdueix el teu 2r cognom: ")
         codi = cognom1[:2] + cognom2[:2] + nom[:2]
         lst.append(codi)
+        codilst.append(codi)
 
         telefon = input("Introdueix el teu número de telèfon: ")
         while len(telefon) < 9 or len(telefon) > 9:
             telefon = input("Introdueix el teu número de telèfon: ")
         lst.append(telefon)
+        tflst.append(telefon)
 
         edad = int(input("Introdueix la teva edad: "))
         while edad < 1 or edad > 123:
             edad = int(input("Introdueix la teva edad: "))
         lst.append(edad)
+        edadlst.append(edad)
 
         contacte = int(input("Introdueix si és un contacte nou o no: "))
         while contacte < 0 or contacte > 1:
             contacte = int(input("Introdueix si és un contacte nou o no: "))
         lst.append(contacte)
-
-        codilst.append(codi)
-        tflst.append(telefon)
-        edadlst.append(edad)
         contactolst.append(contacte)
 
+        # valor = input("Introdueix la paraula YES o NO: ")
+        # if valor[:1] == 'y':
+        #     print('y')
+        # else:
+        #     print('n')
+        # lst.append(valor)
+        # valorlst.append(valor)
+
     while i < len(codilst):
-        print("\t",codilst[i],"\t",tflst[i],"\t",edadlst[i],"\t",contactolst[i])
-        i += 1
+       print("\t",codilst[i],"\t",tflst[i],"\t",edadlst[i],"\t",contactolst[i],)
+       i += 1
 
 
 if __name__ == '__main__':
