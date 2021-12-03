21ce8b568649f8b4a5b21127937d9bf53dbec617
juanjo-DAW <jarnau@institutmvm.cat>
2021-12-03 16:01:49 +0100

    Initial code + Incompleto, falta  mostrarlos como una tabla.

--- main2.py
+++ main2.py
@@ -5,28 +5,45 @@
 
 def main():
     lst = list()
+    i = 0
+    j = 0
+    k = 6
     registres = int(input("Introdueix el nombre de valors del teus registres: "))
     while registres < 1:
         registres = int(input("Introdueix el nombre de valors del teus registres: "))
 
     for x in range(registres):
         nom = input("Introdueix el teu Nom:")
+        lst.append(nom)
         cognom1 = input("Introdueix el teu 1r cognom: ")
+        lst.append(cognom1)
         cognom2 = input("Introdueix el teu 2r cognom: ")
+        lst.append(cognom2)
 
         telefon = input("Introdueix el teu número de telèfon: ")
-        while len(telefon) > 9 or len(telefon) < 9:
+        while len(telefon) < 9 or len(telefon) > 9:
             telefon = input("Introdueix el teu número de telèfon: ")
+        lst.append(telefon)
 
         edad = int(input("Introdueix la teva edad: "))
-        while edad > 123 or edad < 1:
+        while edad < 1 or edad > 123:
             edad = int(input("Introdueix la teva edad: "))
+        lst.append(edad)
+
+        contacte = int(input("Introdueix si és un contacte nou o no: "))
+        while contacte < 0 or contacte > 1:
+            contacte = int(input("Introdueix si és un contacte nou o no: "))
+        lst.append(contacte)
+
+    while i < registres:
+        print( lst[j:k])
+        i += 1
+        j += 6
+        k += 6
 
 
-
-    for x in range(registres):
-        print("El teu codi és: ", cognom1[:2] + cognom2[:2] + nom[:2])
-
+    # for x in range(registres):
+    #     print("El teu codi és: ", cognom1[:2] + cognom2[:2] + nom[:2])
 
 if __name__ == '__main__':
     main()
