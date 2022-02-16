28b04346c3270773b5574c58b9259738b39c8d24
juanjo-DAW <95426160+juanjo-DAW@users.noreply.github.com>
2022-02-16 16:58:08 +0100

    Delete Funciones directory

--- Funciones/main3.py
+++
@@ -1,9 +0,0 @@
-import funcions
-TEXTO_1 = "Introduce un numero:"
-def main():
-    num = funcions.validarint(TEXTO_1)
-    bin = funcions.binary(num)
-    print(bin)
-
-if __name__ == '__main__':
-    main()
